c9e3b4d61a474024fbb88e43829dd1244d743931
Patrick Crane <pcrane89@gmail.com>
2013-10-09 20:05:51 -0400

    Add watch file dialog.

--- src/main/java/com/selesse/tailerswift/ui/menu/FileMenu.java
+++ src/main/java/com/selesse/tailerswift/ui/menu/FileMenu.java
@@ -7,17 +7,47 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.io.File;
 
 public class FileMenu {
     private JMenu jMenu;
+    private JFileChooser jFileChooser;
+    private JMenuItem addWatchedFileMenuItem;
+    private JMenuItem exitMenuItem;
 
     public FileMenu() {
         this.jMenu = new JMenu("File");
         if (Program.getInstance().getOperatingSystem() != OperatingSystem.MAC) {
-            jMenu.add(createExitJMenuItem());
+            jFileChooser = new JFileChooser();
+
+            addWatchedFileMenuItem = createAddWatchedFileJMenuItem();
+            jMenu.add(addWatchedFileMenuItem);
+
+            exitMenuItem = createExitJMenuItem();
+            jMenu.add(exitMenuItem);
         }
+    }
+
+    private JMenuItem createAddWatchedFileJMenuItem() {
+        JMenuItem addWatchedFileMenuItem = new JMenuItem("Watch File...");
+        addWatchedFileMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W,
+                Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
+        addWatchedFileMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int returnVal = jFileChooser.showDialog(null, "Select File");
+
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    File file = jFileChooser.getSelectedFile();
+
+                } else {
+                    // canceled
+                }
+            }
+        });
+
+        return addWatchedFileMenuItem;
     }
 
     private JMenuItem createExitJMenuItem() {
@@ -37,4 +67,5 @@ public class FileMenu {
     public JMenu getJMenu() {
         return jMenu;
     }
+
 }
