98427defd510e9868d84e55c2ec558704573dde3
Alex Selesse <alex@selesse.com>
2014-01-29 21:11:16 -0500

    Include licenses in "About" dialog.

--- src/main/java/com/selesse/tailerswift/gui/view/AboutFrame.java
+++ src/main/java/com/selesse/tailerswift/gui/view/AboutFrame.java
@@ -1,27 +1,85 @@
 package com.selesse.tailerswift.gui.view;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
 
 public class AboutFrame extends JFrame {
-    private JLabel aboutLabel = new JLabel("http://github.com/selesse/tailer-swift");
-    private JLabel disclaimer = new JLabel("This project uses open source software:");
+    private static Logger logger = LoggerFactory.getLogger(AboutFrame.class);
 
     public AboutFrame() {
         super("About");
         this.setName("About");
-        this.setPreferredSize(new Dimension(500, 500));
+        this.setPreferredSize(new Dimension(600, 700));
+        this.setLayout(new GridBagLayout());
 
-        this.setLayout(new BorderLayout());
-        JPanel panel = new JPanel(new BorderLayout());
-        aboutLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        disclaimer.setHorizontalAlignment(SwingConstants.CENTER);
-        panel.add(aboutLabel, BorderLayout.NORTH);
-        panel.add(disclaimer, BorderLayout.SOUTH);
-        this.add(panel, BorderLayout.NORTH);
-        this.add(new JTextArea("Software licenses here"), BorderLayout.CENTER);
+        final JButton urlButton = new JButton("http://github.com/selesse/tailer-swift");
+        urlButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                if (Desktop.isDesktopSupported()) {
+                    try {
+                        Desktop.getDesktop().browse(URI.create(urlButton.getText()));
+                        logger.debug("User clicked on the Github link");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        JLabel openSourceLabel = new JLabel("This project uses open source software:", SwingConstants.CENTER);
+
+        JTextArea licenseTextArea = new JTextArea();
+        licenseTextArea.setText(getOpenSourceLicenseText());
+        JScrollPane scrollPane = new JScrollPane(licenseTextArea, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridy = 0;
+        constraints.insets = new Insets(15, 15, 15, 15);
+        constraints.fill = GridBagConstraints.NONE;
+        this.add(urlButton, constraints);
+
+        constraints.gridy = 1;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        this.add(openSourceLabel, constraints);
+
+        constraints.gridy = 2;
+        constraints.gridheight = 10;
+        constraints.weighty = 1.0;
+        constraints.weightx = 1.0;
+        constraints.fill = GridBagConstraints.BOTH;
+        this.add(scrollPane, constraints);
+
 
         this.pack();
         this.setLocationRelativeTo(null);
+    }
+
+    private String getOpenSourceLicenseText() {
+        String licenseText = "";
+        URL guavaUrl = Resources.getResource("licenses/guava.txt");
+        URL slf4jUrl = Resources.getResource("licenses/slf4j.txt");
+
+        try {
+            String guavaContents = Resources.toString(guavaUrl, Charsets.UTF_8);
+            String slf4jContents = Resources.toString(slf4jUrl, Charsets.UTF_8);
+
+            licenseText = guavaContents + "\n\n" + slf4jContents;
+        } catch (IOException e) {
+            logger.error("Could not open license: {}", e);
+        }
+
+        return licenseText;
     }
 }
